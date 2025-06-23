d08f017ea7fd2052fa22e14d97149aa2edf7013b
czy <czypant17@gmail.com>
2025-06-22 20:38:03 -0500

    Clean up build artifacts and update gitignore
    - Remove deleted expo files from lib/
    - Add package-lock.json to .gitignore
    - Prepare for proper version bump

--- lib/expo.d.ts
+++
@@ -1,9 +0,0 @@
-export { DatalyrSDK as datalyr } from './datalyr-sdk';
-export * from './types';
-export { attributionManager } from './attribution';
-export { createAutoEventsManager, AutoEventsManager } from './auto-events';
-export { debugLog, errorLog, generateUUID, Storage, getOrCreateVisitorId, getOrCreateSessionId, createFingerprintData, getNetworkType, validateEventName, validateEventData, isFirstLaunch, checkAppVersion } from './utils-expo';
-export * from './http-client';
-export * from './event-queue';
-import { DatalyrSDK } from './datalyr-sdk';
-export default DatalyrSDK;
